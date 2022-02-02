75bcad95d59db80715bae3b52a83db255b1d1599
Laith Harb <laithharb@Laiths-iMac.local>
2022-02-02 10:17:19 -0500

    book

--- book_api/apps.py
+++ book_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AnimalsAppConfig(AppConfig):
+class BookApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'animals_app'
+    name = 'book_api'
